63bea034c02aceab50261cd31b6d235599470243
root <jphulck360@hotmail.com>
2017-10-25 22:25:13 -0200

    at_1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,11 +42,11 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
-  #config.action_cable.url = 'wss://jpchattest.herokuapp.com'
-  #config.action_cable.allowed_request_origins = [ 'https://jpchattest.herokuapp.com', 'https://jpchattest.herokuapp.com' ]
+  config.action_cable.url = 'wss://jpchattest.herokuapp.com'
+  config.action_cable.allowed_request_origins = [ 'https://jpchattest.herokuapp.com', 'https://jpchattest.herokuapp.com' ]
 
-  config.action_cable.url = 'wss://jpcompchat.herokuapp.com'
-  config.action_cable.allowed_request_origins = [ 'https://jpcompchat.herokuapp.com', 'https://jpcompchat.herokuapp.com' ]
+  #config.action_cable.url = 'wss://jpcompchat.herokuapp.com'
+  #config.action_cable.allowed_request_origins = [ 'https://jpcompchat.herokuapp.com', 'https://jpcompchat.herokuapp.com' ]
 
 
 
